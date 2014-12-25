5a126759e8583f6bdd37ff558aef90313dbed316
jmjoy <918734043@qq.com>
2014-12-25 21:06:11 +0800

    意外提交

---
+++ Application/Common/Conf/debug.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+		/* 调试 */
+		'SHOW_ERROR_MSG'	=>  true,	// 显示错误信息
+		'SHOW_PAGE_TRACE'	=>	true,	// 显示页面Trace信息		
+);
---
+++ Application/Home/Controller/StudyController.class.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Home\Controller;
+use Think\Controller;
+
+class StudyController extends Controller {
+	
+    public function index(){
+		$this->display();
+    }
+
+}
