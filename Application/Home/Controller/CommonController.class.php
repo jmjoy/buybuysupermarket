3e47a0e9fcfdbe128d040ad20a87824591714b04
jmjoy <918734043@qq.com>
2015-01-01 01:36:15 +0800

    调整了一下base.html

--- Application/Home/Controller/CommonController.class.php
+++ Application/Home/Controller/CommonController.class.php
@@ -6,8 +6,6 @@ use Think\Controller;
 class CommonController extends Controller {
 	
 	public function _initialize() {
-		$this->web_name = '买买网';
-		$this->web_title = '买买网，快来买啊！';
 	}
 	
 	public function test() {
