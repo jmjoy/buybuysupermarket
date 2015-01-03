d2b7a19083c2258ac3b0969fe7b5861b9df91772
jmjoy <918734043@qq.com>
2015-01-03 23:21:06 +0800

    添加easyui

--- Application/Admin/Controller/SettingController.class.php
+++ Application/Admin/Controller/SettingController.class.php
@@ -5,6 +5,18 @@ use Admin\Controller\AuthController;
 
 class SettingController extends AuthController {
 
+	public function listAdmin() {
+		$this->display();
+	}
+	
+	public function listAuth() {
+		$this->display();
+	}
+	
+	public function listRole() {
+		$this->display();
+	}
+	
 	public function info() {
 		$this->display();
 	}
--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -7,15 +7,19 @@ class TestController extends CommonController {
 	
 	//TODO 这里很难的
     public function index(){
-    	
-		if ($img = S('img')) {
-			header('Content-Type: image/jpeg');
-			echo $img;
-		} else {
-			S('img', file_get_contents('./Public/img/default_avatar.jpg'), 60);
-			echo "has saved!";
-		}
-		
+		if (!isset($hello)) {
+			echo "hello";
+		}		
     }
     
 }
+
+
+// 缓存图片，证实可行
+// if ($img = S('img')) {
+// 	header('Content-Type: image/jpeg');
+// 	echo $img;
+// } else {
+// 	S('img', file_get_contents('./Public/img/default_avatar.jpg'), 60);
+// 	echo "has saved!";
+// }
